dcf610f28b4d96f94bfc7dd872f5af005360c2c4
LeeHanByul <byul9123@gmail.com>
2024-05-23 19:14:03 +0900

    댓글 수정 삭제

--- articles/models.py
+++ articles/models.py
@@ -23,15 +23,12 @@ class Article(models.Model):
         return self.title
 
 class Comment(models.Model):
-    Article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name="comments")
-    user = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="comments"
+    article = models.ForeignKey(
+        Article, on_delete=models.CASCADE, related_name="comments"    
     )
-    content = models.CharField(max_length=255)
+    content = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    def __str__(self):
-        return self.content
         
 
 class Like(models.Model):
--- articles/urls.py
+++ articles/urls.py
@@ -11,8 +11,8 @@ urlpatterns = [
     path('Leisure/<int:pk>/', views.LeisureDetail.as_view(), name='Leisure_detail'),
     path('Cooking/', views.CookingList.as_view(), name='Cooking_list'),
     path('Cooking/<int:pk>/', views.CookingDetail.as_view(), name='Cooking_detail'),
-    path('<int:article_pk>/comments/', views.CommentGetPost.as_view(), name='comments-create'),
-    path('<int:article_pk>/comments/<int:comment_pk>/', views.CommentPutDelete.as_view(), name='comments-create'),
+    path('<int:pk>/comments/', views.CommentGetPost.as_view(), name='comments-create'),
+    path('comments/<int:comment_pk>/', views.CommentPutDelete.as_view(), name='comments-create'),
     # #좋아요
     path('like/<int:article_id>/', views.LikeCreate.as_view(), name='like-create'),
 
