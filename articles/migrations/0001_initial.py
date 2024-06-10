14cbd2d28d9ad4dbe6aa32e56e12a0d3f8395e63
Ubuntu <ubuntu@ip-172-31-3-250.ap-northeast-2.compute.internal>
2024-06-10 05:15:58 +0000

    migrate update

--- articles/migrations/0001_initial.py
+++ articles/migrations/0001_initial.py
@@ -38,7 +38,8 @@ class Migration(migrations.Migration):
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('created_at', models.DateTimeField(auto_now_add=True)),
                 ('article', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='likes', to='articles.article')),
-            ],
+                ('user', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
+                ],
             options={
                 'unique_together': {('user', 'article')},
             },
