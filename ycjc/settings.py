587bc2d301aca886f3ed6e61619432babaa094fb
JeongYoonGi <jygmyjesus@nate.com>
2024-06-10 11:33:17 +0900

    인덱스 페이지

--- ycjc/settings.py
+++ ycjc/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = 'django-insecure-4+99yz^bz5=+qcv+(bn4!h+&kku=7^(@l=37yr_#$j_h^6c_hv'
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
