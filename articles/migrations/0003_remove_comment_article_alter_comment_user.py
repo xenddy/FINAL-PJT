1f0ab2e33f769d46412aaea59dbca4d723fa5976
xenddy <xenddy@naver.com>
2024-06-13 10:09:43 +0900

    마이그레이션 의존성 수정

--- articles/migrations/0003_remove_comment_article_alter_comment_user.py
+++ articles/migrations/0003_remove_comment_article_alter_comment_user.py
@@ -9,7 +9,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('articles', '0002_initial'),
+        ('articles', '0001_initial'),
     ]
 
     operations = [
